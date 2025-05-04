9aaee83f348505b842f5b2d43c2b4f6a4e329ef6
ISHIMWE Deborah <ishimwedebby53@gmail.com>
2025-05-04 09:01:31 +0200

    first committ

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -10,6 +10,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
+
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
